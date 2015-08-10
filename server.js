93d1f986f66af77dae527373ba05505a1cf58a32
caulkerman <ih8blackwidowspiders@yahoo.com>
2015-08-10 13:24:06 -0600

    update

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 //
 // DEPENDENCIES///
 var express = require("express");
-var bodyParser = ("body-parser");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+var request = require("request");
+var twilio = require("twilio");
 
 var app = express();  //set our express function call to app so we can use app.kdjflaj later.
-var port = 6666;  //create port///
+var port = 6606;  //create port.  Just so you know, it did not like 6666, almost any other seemed to work///
+
+/////MIDDLEWARE////////
+app.use(express.static("public"));  //
+// app.use(bodyParser.urlencoded( {
+// 	extended:true
+// }));  if it sends a depreciated error, use this code.
+app.use(bodyParser.json());
+app.use(cors());
+
+//////END POINTS///////
+var message = {"message": "HELLO WORLD!!!!!"}
+app.get("/api/message", function(req, res) {
+	return res.json(message);
+});
+app.post("/api/send_text_message", function(req, res) {
+	console.log(req.body.message);
+	res.send();
+})
+
+
 // we want it to listen to out port.
 app.listen(port, function() {
 	console.log("I am watching you", port);
